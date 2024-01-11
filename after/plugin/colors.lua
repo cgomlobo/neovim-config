c1ce68ef0ccab3b0f7ed0bbf1d8aafceca7c49c2
Carlos Gómez-Lobo <cgomlobo@amazon.com>
2024-01-11 10:51:42 +0100

    Add monokai theme

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,6 +1,20 @@
-function applyColor(color)
-    color = color or "rose-pine"
+local colorConfigs = {
+    monokai = function()
+        vim.cmd.highlight({ "Normal", "ctermfg=231", "ctermbg=235", "cterm=NONE", "guifg=#f8f8f2", "guibg=#161616",
+            "gui=NONE" })
+        vim.cmd.highlight({ "LineNr", "ctermfg=102", "ctermbg=237", "cterm=NONE", "guifg=#90908a", "guibg=#262626",
+            "gui=NONE" })
+        vim.cmd.highlight({ "SignColumn", "ctermfg=NONE", "ctermbg=237", "cterm=NONE", "guifg=NONE", "guibg=#262626",
+            "gui=NONE" })
+    end
+}
+
+local function applyColor(color)
+    color = color or "monokai"
     vim.cmd.colorscheme(color)
+
+    local colorConfig = colorConfigs[color]
+    if (colorConfig) then colorConfig() end
 end
 
 applyColor()
--- lua/cgomlobo/packer.lua
+++ lua/cgomlobo/packer.lua
@@ -1,47 +1,48 @@
 return require('packer').startup(function(use)
-  -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
+    -- Packer can manage itself
+    use 'wbthomason/packer.nvim'
 
-  use {
-	  'nvim-telescope/telescope.nvim', tag = '0.1.5',
-	  -- or                            , branch = '0.1.x',
-	  requires = { {'nvim-lua/plenary.nvim'} }
-  }
+    use {
+        'nvim-telescope/telescope.nvim', tag = '0.1.5',
+        -- or                            , branch = '0.1.x',
+        requires = { { 'nvim-lua/plenary.nvim' } }
+    }
 
-  use {
-	  'rose-pine/neovim',
-	  as = 'rose-pine',
-	  config = function()
-		  vim.cmd('colorscheme rose-pine')
-	  end
-  }
+    use {
+        'ku1ik/vim-monokai',
+        as = 'monokai',
+        config = function()
+            vim.cmd('syntax enable')
+            vim.cmd('colorscheme monokai')
+        end
+    }
 
-  use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
+    use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
 
-  use 'ThePrimeagen/harpoon'
+    use 'ThePrimeagen/harpoon'
 
-  use 'tpope/vim-fugitive'
+    use 'tpope/vim-fugitive'
 
-  use 'wuelnerdotexe/vim-astro'
+    use 'wuelnerdotexe/vim-astro'
 
-  use 'm4xshen/autoclose.nvim'
+    use 'm4xshen/autoclose.nvim'
 
-  use 'lukas-reineke/lsp-format.nvim'
+    use 'lukas-reineke/lsp-format.nvim'
 
-  use {
-  'VonHeikemen/lsp-zero.nvim',
-  branch = 'v3.x',
-  requires = {
-    --- Uncomment these if you want to manage LSP servers from neovim
-    {'williamboman/mason.nvim'},
-    {'williamboman/mason-lspconfig.nvim'},
+    use {
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v3.x',
+        requires = {
+            --- Uncomment these if you want to manage LSP servers from neovim
+            { 'williamboman/mason.nvim' },
+            { 'williamboman/mason-lspconfig.nvim' },
 
-    -- LSP Support
-    {'neovim/nvim-lspconfig'},
-    -- Autocompletion
-    {'hrsh7th/nvim-cmp'},
-    {'hrsh7th/cmp-nvim-lsp'},
-    {'L3MON4D3/LuaSnip'},
-  }
-}
+            -- LSP Support
+            { 'neovim/nvim-lspconfig' },
+            -- Autocompletion
+            { 'hrsh7th/nvim-cmp' },
+            { 'hrsh7th/cmp-nvim-lsp' },
+            { 'L3MON4D3/LuaSnip' },
+        }
+    }
 end)
