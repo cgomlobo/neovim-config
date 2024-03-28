96dbdf7d9bbcb6400b076292b8b1e813b01b5b68
Carlos Gómez-Lobo <carlos@gomez-lobo.com>
2024-03-28 23:12:26 +0100

    Improve installation script

--- scripts/neovim_install.sh
+++ scripts/neovim_install.sh
@@ -3,7 +3,7 @@
 TMP=$HOME/.local/neovim_tmp
 INSTALLATION_DIR=$HOME/.local/neovim
 BASEDIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && cd .. && echo `pwd`)
-HOOKS_SCRIPT="${BASEDIR}/.githooks/post-merge"
+HOOKS_DIR="${BASEDIR}/.githooks"
 
 
 function clean() {
@@ -73,18 +73,105 @@ echo "
 export PATH=$INSTALLATION_DIR/bin:\$PATH
 " >> $CONFIG_FILE
 
+export PATH=$INSTALLATION_DIR/bin:$PATH
+
 cd $BASEDIR
 
 echo "[+] Installing plugins..."
 
+# Fetch packer code
+mkdir -p ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+
+# Install plugins using post-merge hook
 mv after after_bak
 mv init.lua init.lua.bak
 
-bash $HOOKS_SCRIPT > /dev/null
+bash $HOOKS_DIR/post-merge > /dev/null
 
 mv after_bak after
 mv init.lua.bak init.lua
 
 clean
+
+read -p "[+] Do you want to set up the hooks? [y/N] " answer
+
+answer=$(echo "$answer" | tr '[:upper:]' '[:lower:]')
+
+if [[ $answer == "yes" ]] || [[ $answer == "y" ]]; then
+    echo "[#] Setting up hooks..."
+    bash $HOOKS_DIR/set_hooks.sh
+fi
+
+function install_language_servers() {
+    # Typescript
+    read -p "[+] Install tsserver? (requires npm) [y/N] " answer
+
+    answer=$(echo "$answer" | tr '[:upper:]' '[:lower:]')
+
+    if [[ $answer == "yes" ]] || [[ $answer == "y" ]]; then
+        npm install -g typescript typescript-language-server
+        echo "[#] tsserver installed"
+    fi
+
+    # Python
+    read -p "[+] Install jedi-language-server? (requires python) [y/N] " answer
+
+    answer=$(echo "$answer" | tr '[:upper:]' '[:lower:]')
+
+    if [[ $answer == "yes" ]] || [[ $answer == "y" ]]; then
+        pip install -U jedi-language-server
+        echo "[#] jedi-language-server installed"
+    fi
+
+    # Rust
+    read -p "[+] Install rust-analyzer? (requires rustup) [y/N] " answer
+
+    answer=$(echo "$answer" | tr '[:upper:]' '[:lower:]')
+
+    if [[ $answer == "yes" ]] || [[ $answer == "y" ]]; then
+        rustup component add rust-analyzer
+        echo "[#] rust-analyzer installed"
+    fi
+
+    # Emmet
+    read -p "[+] Install emmet-ls? (requires npm) [y/N] " answer
+
+    answer=$(echo "$answer" | tr '[:upper:]' '[:lower:]')
+
+    if [[ $answer == "yes" ]] || [[ $answer == "y" ]]; then
+        npm install -g emmet-ls
+        echo "[#] emmet-ls installed"
+    fi
+
+    # Astro
+    read -p "[+] Install @astrojs/language-server? (requires npm) [y/N] " answer
+
+    answer=$(echo "$answer" | tr '[:upper:]' '[:lower:]')
+
+    if [[ $answer == "yes" ]] || [[ $answer == "y" ]]; then
+        npm install -g @astrojs/language-server
+        echo "[#] @astrojs/language-server installed"
+    fi
+
+    # Svelte
+    read -p "[+] Install svelte-language-server? (requires npm) [y/N] " answer
+
+    answer=$(echo "$answer" | tr '[:upper:]' '[:lower:]')
+
+    if [[ $answer == "yes" ]] || [[ $answer == "y" ]]; then
+        npm install -g @svelte-language-server
+        echo "[#] svelte-language-server installed"
+    fi
+}
+
+# Install language servers
+read -p "[+] Do ou want to install language servers? [y/N] " answer
+
+answer=$(echo "$answer" | tr '[:upper:]' '[:lower:]')
+
+if [[ $answer == "yes" ]] || [[ $answer == "y" ]]; then
+    install_language_servers
+fi
 
 echo "[+] Done! Run 'source $CONFIG_FILE' to refresh the PATH."
