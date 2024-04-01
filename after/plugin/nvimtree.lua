2159e2264aeb23ed5e2de03cf0f040348c7293c9
Carlos Gómez-Lobo <cgomlobo@amazon.com>
2024-04-01 18:29:45 +0200

    Tweak keymaps and fix autosave

--- after/plugin/nvimtree.lua
+++ after/plugin/nvimtree.lua
@@ -2,6 +2,38 @@ local HEIGHT_RATIO = 0.8
 local WIDTH_RATIO = 0.4
 
 require("nvim-tree").setup({
+    on_attach = function(bufnr)
+        local api = require("nvim-tree.api")
+        local opts = function(desc)
+            return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+        end
+
+        local git_add = function()
+            local node = api.tree.get_node_under_cursor()
+            local gs = node.git_status.file
+
+            -- If the current node is a directory get children status
+            if gs == nil then
+                gs = (node.git_status.dir.direct ~= nil and node.git_status.dir.direct[1])
+                    or (node.git_status.dir.indirect ~= nil and node.git_status.dir.indirect[1])
+            end
+
+            -- If the file is untracked, unstaged or partially staged, we stage it
+            if gs == "??" or gs == "MM" or gs == "AM" or gs == " M" then
+                vim.cmd("silent !git add " .. node.absolute_path)
+
+                -- If the file is staged, we unstage
+            elseif gs == "M " or gs == "A " then
+                vim.cmd("silent !git restore --staged " .. node.absolute_path)
+            end
+
+            api.tree.reload()
+        end
+
+        api.config.mappings.default_on_attach(bufnr)
+        vim.keymap.set("n", "t", api.tree.toggle_gitignore_filter, opts('Toggle git filter'))
+        vim.keymap.set('n', 'ga', git_add, opts('Git Add'))
+    end,
     view = {
         float = {
             enable = true,
--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -2,6 +2,5 @@ local builtin = require('telescope.builtin')
 
 vim.keymap.set('n', '<leader>o', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.git_files, {})
-vim.keymap.set('n', '<leader>s', function()
-    builtin.live_grep()
-end)
+vim.keymap.set('n', '<leader>fa', function() builtin.find_files({ no_ignore = true }) end, {})
+vim.keymap.set('n', '<leader>s', function() builtin.live_grep() end)
