47e680743cad084669c139b0bbd5dcbc141ff07f
Carlos Gómez-Lobo <cgomlobo@amazon.com>
2024-04-02 11:59:46 +0200

    Fix autosave

--- after/plugin/autosave.lua
+++ after/plugin/autosave.lua
@@ -1,7 +1,7 @@
 vim.api.nvim_create_autocmd({ "BufLeave", "FocusLost", "BufWinLeave" }, {
     pattern = "*",
     callback = function()
-        if (vim.fn.isdirectory(vim.fn.expand('%')) or vim.bo.buftype ~= "" or vim.fn.expand("%") == "") then
+        if (vim.bo.buftype ~= "" or vim.fn.expand("%") == "") then
             return
         end
         vim.api.nvim_command('write')
