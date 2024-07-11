c9cace56fcb18bc7ab89ce082372b8f96260d3cf
Carlos Gómez-Lobo <cgomlobo@amazon.com>
2024-07-11 12:58:16 +0200

    Make harpoon window bigger

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -1,3 +1,8 @@
+require('harpoon').setup({
+    menu = {
+        width = math.floor(vim.api.nvim_win_get_width(0) * 0.5)
+    }
+})
 local mark = require('harpoon.mark')
 local ui = require('harpoon.ui')
 
